024c9568bd7956614fc1f74626d9ce919bd9d435
KunchamAkshaya <kunchamakshaya@gmail.com>
2024-05-20 16:37:00 +0530

    Add files via upload

---
+++ Simplecal.py
@@ -0,0 +1,14 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter 2nd number: "))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a**b
+h=a//b
+print("addition:",c)
+print("Multiplication:",e)
+print("Division:",f)
+print("Subtraction:",d)
+print("Exponent:",g)
+print("Floor Division:",h)
